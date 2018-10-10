aa5a83af0256713fdce2c2e24445239d83557962
moogen <jwu9995@gmail.com>
2018-10-09 19:41:33 -0500

    Changed to stream Pages

--- Client/client.py
+++ Client/client.py
@@ -28,6 +28,15 @@ class Topic_Model():
     	return self.stub.topicModel(req)
 
 class Journal():
+	""" A representation of a BHL Scientific Journal. Contains a collection of the names and text that exists in the journal. 
+
+	Class Attributes: 
+	title - the title of the journal 
+	num_pages - the number of pages in the journal
+	pages_to_names - a dictionary that maps from the page in the journal (indexed from 0) to a list of names present on that page 
+	pages_to_text - a dictionary that maps from the page in the journal (indexed from 0) to the text present on that page
+					For now, the text is not processed at all before stored in the dictionary (i.e., still has whitespace characters)
+	""" 
 	def __init__(self, title): 
 		self.title = title
 		self.num_pages = 0 
@@ -55,13 +64,50 @@ class Journal():
 			self.num_pages += 1 
 		self.pages_to_names[page_number] = names 
 
-def check_journal_name(journal1, journal2): 
-	""" Compares the names of two page.title_id strings to see if they are from the same journal 
-	
-	Simply compares the first 5 characters in the string - if they are the same, 
-	""" 
-	pass
+def break_down_doc(doc_list):  
+    reqs = []
+    for doc in doc_list:	
+        reqs.append(my_service_pb2.QueryRequest(page=doc))
+    for req in reqs:
+        yield req
 
+def Ver(stub): 
+	return stub.Ver(protob_pb2.Void()).value
+
+def Pages(stub, withText = 1, titleIds = []): 
+	""" Retrieves the pages belonging to the requested journals and streams it back to the caller 
+
+	By default, we stream all of the pages with their associated text
+
+	Inputs:
+	withText - Either 0 or 1, indicating whether or not we want to stream the pages with their associated text 
+	titleIds - A list of journal ids that we are interested in. These correspond to the indices of the journals in the database
+	""" 
+	pagesOpt = protob_pb2.PagesOpt(with_text = withText, title_ids = titleIds) 
+	pages = stub.Pages(pagesOpt) # A stream of pages 
+
+	prev_title = "" 
+	first = True 
+	journal = None 
+
+	for page in pages: 
+		if page.title_id != prev_title: 
+			if not first: # Yield what we have currently before creating a new journal 
+				yield journal 
+			else: 
+				first = False 
+			journal = Journal(page.title_id) # Create a new journal 
+			prev_title = page.title_id 
+			page_num = 1
+		if withText == 1: 
+			journal.add_page(page_num, page.names, str(page.text))
+		else: 
+			journal.add_names(page_num, page.names)
+		page_num += 1
+
+"""
+# Old code that uses the previous Protocol Buffer definition
+# May be useful as reference 
 def pull_journals(stub, num_journals, with_text = 0):
 	wt = protob_pb2.WithText(value = with_text)
 	pages = stub.Pages(wt) 
@@ -84,60 +130,33 @@ def pull_journals(stub, num_journals, with_text = 0):
 			journals[index].add_names(page_num, page.names) # Hand-waving the page number 
 		page_num += 1
 	return journals
+"""
 
-def break_down_doc(doc_list):  
-    reqs = []
-    for doc in doc_list:	
-        reqs.append(my_service_pb2.QueryRequest(page=doc))
-    for req in reqs:
-        yield req
+def Titles(stub): 
+	""" Implements the Titles rpc defined in protob.proto 
 
-def Ver(stub): 
-	return stub.Ver(protob_pb2.Void()).value
-
-def Pages(stub, with_text = 1):
-	wt = protob_pb2.WithText(value = with_text)
-	pages = stub.Pages(wt)
-	title_id = ""
-	i = 0
-	doc_list = []
-	title_list = []
-	document = ""
-	for page in pages:
-		# print(page.title_id)
-		temp_title_id = page.title_id
-		document += str(page.text)
-		if temp_title_id != title_id:
-			i  = i + 1
-			title_id = temp_title_id
-			doc_list.append(document)
-			title_list.append(title_id)
-			# file_name = "Output" + str(i) + ".txt"
-			# text_file = open(file_name, "w")
-			# text_file.write(document)
-			# text_file.close()
-			document = ""
-		if(i >= 100):
-			break
-	# tm = Topic_Model()
-	# tm.train(doc_list, 3, 5)
-	# tm.tf_idf()
-	# tm.lda() 
-	print(title_list)
-	return doc_list
+	Sends no parameters to the server and receives a list of all of the Journal titles that exist on the server's database 
+	Each Title received from the server contains a unique id, its archive_id, its filepath on the server, and the language 
+	the Journal was written in. 
+	""" 
+	titles = stub.Titles(protob_pb2.TitlesOpt())
+	for title in titles:
+		yield title
 
 def run_client(): 
 	doc_list = []
 	host = '172.22.247.23:8888'
 	with grpc.insecure_channel(host) as channel: 
 		stub = protob_pb2_grpc.BHLIndexStub(channel)
-
-		Ver(stub)
+		for title in Titles(stub):
+			print(title)
+		
+		# Ver(stub)
 		# Pages(stub, with_text = 1)
 		# doc_list = Pages(stub, with_text = 1)
-		journals = pull_journals(stub, 100, with_text = 0)
-		vis = plotly_example.Plotly_Visualization(tile_number = 1, tile_width = 10, tile_height = 10, journal_list = journals)
-		vis.visualize_data()
+		# journals = pull_journals(stub, 100, with_text = 0)
+		# vis = plotly_example.Plotly_Visualization(tile_number = 1, tile_width = 10, tile_height = 10, journal_list = journals)
+		# vis.visualize_data()
 		
 	""" 
 	print("Documents recieved from " + host)
